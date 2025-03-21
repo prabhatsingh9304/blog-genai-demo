db9f18cc9267721965a5a1a0c43f937169a51924
prabhatsingh9304 <exploitdev0@gmail.com>
2025-03-21 23:18:00 +0530

    adde base and find_link script

---
+++ agent/base.py
@@ -0,0 +1,7 @@
+#make a dummy list of trending keywords
+#create a prompt templete to find one relavent keyword
+#make dummy list of blogs which contains paragraphs(crawled pages)
+#make rag of it
+#create system Message for creating the blog
+#Create blog using userpromt and rag
+ 
